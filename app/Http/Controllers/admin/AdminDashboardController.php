755c133995403eb32da59ceb5cce4b4f4b77d33b
syar <shamseramsyar@gmail.com>
2026-02-11 21:12:49 +0800

    dashboard

--- app/Http/Controllers/admin/AdminDashboardController.php
+++ app/Http/Controllers/admin/AdminDashboardController.php
@@ -37,7 +37,7 @@ class AdminDashboardController extends Controller
             ->whereIn('selection', ['MESYUARAT', 'PROGRAM', 'KURSUS/BENGKEL'])
             ->count();
 
-        return view('dashboard', compact('totalLecturers', 'totalLecturers', 'sickLeave', 'outsideDuty'));
+        return view('admin.Dashboard', compact('totalLecturers', 'totalLecturers', 'sickLeave', 'outsideDuty'));
     }
 
     public function getRealtimeData()
